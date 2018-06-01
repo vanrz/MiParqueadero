7fee838fc3d4ee047497bf56f66306c537983011
Estudiantes <Estudiantes@Labing70705>
2018-06-01 11:06:50 -0500

    commit 1

---
+++ Parqueadero/src/parqueadero/IVehiculo.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package parqueadero;
+
+/**
+ *
+ * @author Estudiantes
+ */
+  public abstract interface IVehiculo {
+    
+    /**
+     *
+     * @return
+     */
+    private String placa();
+    private String propietario();
+    private float cilindraje();
+    
+    
+    
+    
+}
---
+++ Parqueadero/src/parqueadero/Parqueadero.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package parqueadero;
+
+/**
+ *
+ * @author Estudiantes
+ */
+public class Parqueadero {
+    
+    misVehiculos Vehiculo[100]=new Vehiculo();
+    
+    
+    
+
+    
+    
+}
